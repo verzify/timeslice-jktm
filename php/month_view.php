7d4b03c712b33796fff06034fcb3490c70a50ebc
Kelvin Leong <khleong89@gmail.com>
2012-10-14 01:51:43 +0800

    fixed date problem

---
+++ php/month_view.php
@@ -0,0 +1,50 @@
+﻿<html lang="en">
+	<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+          <?php include 'imports.html'; ?>         
+	  <title>Time Slice - Month View</title>
+      <script type='text/javascript'>
+
+	$(document).ready(function() {
+		
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title'
+			},
+			editable: true,
+			dayClick: function(date, allDay, jsEvent, view) {
+				
+				var day = date.getDate()
+				if(day < 10){
+					day = "0" + day;
+				}	
+				var month = date.getMonth() + 1;
+				
+				if(month < 10){
+					month = "0" + month;
+				}	
+				
+				var year = date.getFullYear();
+       			 window.location = "main.php?selected_date=" + day + month + year;
+    		}
+		});
+		
+	});
+
+</script>  
+	</head>
+	<body>
+        <div data-role="page" style="background-color:white">
+            
+           	<div id="top_banner"></div><!-- top banner --> 
+           	           
+           	<div data-role="content" style="background-color:white">
+				<div id='calendar'></div>
+            </div><!-- /content -->
+            
+            <?php include 'footer.html'; ?>   
+        </div><!-- /page -->
+
+	</body>
+</html>
---
+++ php/week_view.php
@@ -0,0 +1,42 @@
+﻿<html lang="en">
+	<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+          <?php include 'imports.html'; ?>        
+	  <title>Time Slice - Week View</title>
+          
+	  </head>
+	<body>
+        <div data-role="page" style="background-color:white">
+            
+	    	<div id="top_banner"></div><!-- top banner -->
+				
+	        <div data-role="content" style="background-color:white">
+				<div id="weekly_content">
+					<a data-role="button" href="main.html">
+						<img src="images/weekly/monday.jpg" style="width:100%;height:auto" />
+					</a>
+					<a data-role="button" href="main.html">
+						<img src="images/weekly/tuesday.jpg" style="width:100%;height:auto" />
+					</a>
+					<a data-role="button" href="main.html">
+						<img src="images/weekly/wednesday.jpg" style="width:100%;height:auto" />
+					</a>
+					<a data-role="button" href="main.html">
+						<img src="images/weekly/thursday.jpg" style="width:100%;height:auto" />
+					</a>
+					<a data-role="button" href="main.html">
+						<img src="images/weekly/friday.jpg" style="width:100%;height:auto" />
+					</a>
+					<a data-role="button" href="main.html">
+						<img src="images/weekly/saturday.jpg" style="width:100%;height:auto" />
+					</a>
+					<a data-role="button" href="main.html">
+						<img src="images/weekly/sunday.jpg" style="width:100%;height:auto" />
+					</a>
+				</div>
+	        </div><!-- /content -->
+            <?php include 'footer.html'; ?>   
+        </div><!-- /page -->
+
+	</body>
+</html>
