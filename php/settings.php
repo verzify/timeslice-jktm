1d72918cf04ecbeb35d5ba744ff7fabff6a9e214
Timothy Teo <timothy.teo.2010@sis.smu.edu.sg>
2012-10-17 12:42:00 +0800

    Added form into settings.php

--- php/settings.php
+++ php/settings.php
@@ -1,13 +1,35 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <?php include 'imports.html'; ?> 
+        <title>Time Slice - New Event</title>
 
-<head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" x-undefined="" />
-<title>Untitled 1</title>
-</head>
+    </head>
+    <body>
+        <div data-role="page" style="background-color:white">
 
-<body>
+            <div data-role="header" data-position="fixed">
+				<a href="index.php" data-icon="home">Home</a>
+				<h1>New Event</h1>
+				<a href="settings.php" data-icon="gear" class="ui-btn-right">Options</a>
+			</div>
 
-</body>
+            <div data-role="content" style="background-color:white">
+                <div id="form">
+                    <form id="eventForm" action="processEditSettings.php" method="GET">
+                        <label for="eName">Sleeping Hours</label>
+                        <input type="text" name="sleephours" id="sleephours" required="required" value="8"/>
 
+                        <fieldset class="ui-grid-a">
+                            <div class="ui-block-a"><a href="main.php" data-role="button">Cancel</a></div>
+                            <div id="submit" class="ui-block-b"><button type="submit" data-theme="a">Edit</button></div>
+                        </fieldset>
+                    </form>	
+                </div>
+            </div><!-- /content -->
+            <div data-role="footer" data-id="fool" data-position="fixed">
+                <?php include 'footer.html'; ?> 
+            </div><!-- /footer -->
+        </div><!-- /page -->
+    </body>
 </html>
